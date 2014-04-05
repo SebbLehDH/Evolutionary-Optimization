d0ce9a0d284a5567db3629647fc2c33edb811dd8
Seb <sebastian.georg.lehmann@gmx.de>
2014-04-05 15:10:26 +0200

    ertser commit: lokale Optimierung fehlerhaft

---
+++ WindowsFormsApplication1/Evolutionary Algorithms.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_generate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gui_cities = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gui_workers = new System.Windows.Forms.TextBox();
+            this.button_local = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gui_totalLength = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gui_avgLength = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleDescription = "Panel for Visualization";
+            this.panel1.AccessibleName = "DrawPanel";
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(338, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(413, 344);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_generate
+            // 
+            this.button_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button_generate.Location = new System.Drawing.Point(267, 53);
+            this.button_generate.Name = "button_generate";
+            this.button_generate.Size = new System.Drawing.Size(70, 23);
+            this.button_generate.TabIndex = 1;
+            this.button_generate.Text = "generate";
+            this.button_generate.UseVisualStyleBackColor = true;
+            this.button_generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Cities";
+            // 
+            // gui_cities
+            // 
+            this.gui_cities.Location = new System.Drawing.Point(161, 27);
+            this.gui_cities.Name = "gui_cities";
+            this.gui_cities.Size = new System.Drawing.Size(100, 20);
+            this.gui_cities.TabIndex = 3;
+            this.gui_cities.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of employees";
+            // 
+            // gui_workers
+            // 
+            this.gui_workers.Location = new System.Drawing.Point(161, 56);
+            this.gui_workers.Name = "gui_workers";
+            this.gui_workers.Size = new System.Drawing.Size(100, 20);
+            this.gui_workers.TabIndex = 5;
+            this.gui_workers.Text = "2";
+            // 
+            // button_local
+            // 
+            this.button_local.Location = new System.Drawing.Point(15, 130);
+            this.button_local.Name = "button_local";
+            this.button_local.Size = new System.Drawing.Size(85, 24);
+            this.button_local.TabIndex = 6;
+            this.button_local.Text = "single paths";
+            this.button_local.UseVisualStyleBackColor = true;
+            this.button_local.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(335, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total Length";
+            // 
+            // gui_totalLength
+            // 
+            this.gui_totalLength.Enabled = false;
+            this.gui_totalLength.Location = new System.Drawing.Point(565, 380);
+            this.gui_totalLength.Name = "gui_totalLength";
+            this.gui_totalLength.Size = new System.Drawing.Size(100, 20);
+            this.gui_totalLength.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(335, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Average Length per Path";
+            // 
+            // gui_avgLength
+            // 
+            this.gui_avgLength.Enabled = false;
+            this.gui_avgLength.Location = new System.Drawing.Point(565, 406);
+            this.gui_avgLength.Name = "gui_avgLength";
+            this.gui_avgLength.Size = new System.Drawing.Size(100, 20);
+            this.gui_avgLength.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(758, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speichernToolStripMenuItem,
+            this.öffnenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dateiToolStripMenuItem.Text = "DATEI";
+            this.dateiToolStripMenuItem.Click += new System.EventHandler(this.dateiToolStripMenuItem_Click);
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            this.speichernToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+            // 
+            // öffnenToolStripMenuItem
+            // 
+            this.öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+            this.öffnenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.öffnenToolStripMenuItem.Text = "Öffnen";
+            this.öffnenToolStripMenuItem.Click += new System.EventHandler(this.öffnenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "Evolutionary Optimization";
+            this.AccessibleName = "main_window";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(758, 534);
+            this.Controls.Add(this.gui_avgLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gui_totalLength);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_local);
+            this.Controls.Add(this.gui_workers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gui_cities);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_generate);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Evolutionary Optimization";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_generate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox gui_cities;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gui_workers;
+        private System.Windows.Forms.Button button_local;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox gui_totalLength;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gui_avgLength;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öffnenToolStripMenuItem;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Evolutionary Algorithms.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string gv_NoIntMessage = "Please only insert positive Numbers!";
+        public int gv_cities = 0;
+        public int gv_workers = 0;
+        public int gv_citiesPerPath;
+        public int gv_pathLeft;
+        public int midX;
+        public int midY;
+        public double gv_totalLength;
+        public double gv_avgLength;
+        public Point[] fullPath = new Point[0];    // initial definition, resize later and fill with the "paths"
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Paint += new PaintEventHandler(panel1_Paint);
+            panel1.Controls.Clear();
+
+            midX = panel1.Width / 2;
+            midY = panel1.Height / 2;
+        }
+
+        public void panel1_Paint( object sender, PaintEventArgs e)
+        {
+            gv_totalLength = 0;
+            gv_avgLength = 0;
+
+            var p = sender as Panel;
+            var g = e.Graphics;
+
+            Pen blackPen = new Pen( Color.Black);
+            Brush redBrush = new SolidBrush(Color.Red);
+            Brush blackBrush = new SolidBrush(Color.Black);
+
+
+            if (gv_cities == 0 & gv_workers == 0)
+            {
+                g.FillEllipse(redBrush, midX - 2, midY - 2, 5, 5);
+            }
+
+            else
+            {
+                g.FillEllipse(redBrush, midX - 2, midY - 2, 5, 5);
+
+                g.DrawPolygon(blackPen, fullPath);
+                
+                for (int i = 0; i < fullPath.Length-1; i++)
+                {
+                    gv_totalLength += Math.Sqrt(Math.Pow(fullPath[i].X - fullPath[i + 1].X, 2) + Math.Pow(fullPath[i].Y - fullPath[i + 1].Y, 2));
+                }
+                gui_totalLength.Text = gv_totalLength.ToString();
+                gv_avgLength = gv_totalLength / gv_workers;
+                gui_avgLength.Text = gv_avgLength.ToString();
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                gv_cities = int.Parse(gui_cities.Text);
+                gv_workers = int.Parse(gui_workers.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(gv_NoIntMessage);
+            }
+            finally
+            {
+                if (gv_cities <= 0 | gv_workers <= 0)
+                {
+                    MessageBox.Show("Please only use positive numbers");
+                }
+
+                else if (gv_workers > gv_cities)
+                {
+                    MessageBox.Show("Numbers of cities should be higher than the number of emloyees");
+                }
+                else
+                {
+
+                    fullPath = initialization.path(gv_cities, gv_workers, panel1.Width, panel1.Height);
+
+                    gv_totalLength = 0;
+                    gv_avgLength = 0;
+                    this.Refresh();
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fullPath = local_optimization.singlePath(fullPath);
+            this.Refresh();
+        }
+
+        public class initialization
+        {
+            public static Point[] path(int cities, int workers, int width, int height)
+            {
+                Point[] fullPath = new Point[0];
+                Random pointRandom = new Random();
+
+                int citiesPerPath = cities / workers;       
+                int pathLeft = cities % workers;            //modulo leftover
+
+                int midX = width / 2;
+                int midY = height / 2;
+
+                try
+                {
+                    for (int i = 0; i < workers; i++)
+                    {
+
+                        Point[] path = new Point[citiesPerPath + 1];
+
+                        try
+                        {
+                            for (int j = 1; j <= citiesPerPath; j++)
+                            {
+                                Boolean rand = true;
+                                int randomX = 0;
+                                int randomY = 0;
+                                while (rand)
+                                {
+                                    randomX = pointRandom.Next(0, width);
+                                    randomY = pointRandom.Next(0, height);
+
+                                    if (randomX == midX & randomY == midY)
+                                    {
+                                        rand = true;
+                                    }
+                                    else
+                                    {
+                                        rand = false;
+                                    }
+                                    for (int k = 0; k < fullPath.Length; k++)
+                                    {
+                                        if (randomX == fullPath[k].X & randomY == fullPath[k].Y)
+                                        {
+                                            rand = true;
+                                        }
+                                    }
+                                    for (int k = 0; k < fullPath.Length; k++)
+                                    {
+                                        if (randomX == fullPath[k].X & randomY == fullPath[k].Y)
+                                        {
+                                            rand = true;
+                                        }
+
+                                    }
+                                }
+                                path[j] = new Point(randomX, randomY);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("da passt wahrscheinlich was mit dem array nicht");
+                        }
+                        path[0] = new Point(midX, midY);
+                        if (pathLeft > 0)
+                        {
+                            int randomX = pointRandom.Next(0, width);
+                            int randomY = pointRandom.Next(0, height);
+                            pathLeft--;
+                            Array.Resize(ref path, path.Length + 1);
+                            path[citiesPerPath + 1] = new Point(randomX, randomY);
+                        }
+
+                        int initLength = fullPath.Length;           //save Length before resize
+                        Array.Resize(ref fullPath, fullPath.Length + path.Length);      //resize full Array
+
+                        path.CopyTo(fullPath, initLength);          //add single path to complete path
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("da passt wahrscheinlich was mit dem array nicht");
+                    }
+              
+                return fullPath;
+            }
+
+        }
+
+        private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text|*.txt";
+            saveFile.Title = "Save current Path";
+            saveFile.ShowDialog();
+        }
+
+        private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+
+    public class local_optimization
+    {
+        public static Point[] singlePath(Point[] points)
+        {
+
+            int splitX = points[0].X;       //X-coordinate of center
+            int splitY = points[0].Y;       //Y-coordinate of center
+            Boolean leftEnd = true;         //needed to split the complete Array into paths
+
+            Point[] singlePath = new Point[0];      //will be resized and filled with a part of the complete array
+
+            int leftOffset = 0;             //the index to copy from cpmplete Array; initialized
+            int rightOffset = 0;            //the length of the copy from complete Array; initialized
+
+            for (int i = 0; i < points.Length; i++) //loop for splitting the complete Array
+            {
+
+                if (points[i].X == splitX & points[i].Y == splitY)
+                {
+                    if (leftEnd)
+                    {
+                        leftOffset = i;
+                        leftEnd = false;
+                        
+                    }
+                    else
+                    {
+                        rightOffset = i;
+                        leftEnd = true;
+
+                        Array.Resize(ref singlePath, rightOffset+1);
+                        Array.Copy(points, leftOffset, singlePath, 0, rightOffset+1);
+                    }
+                }
+                //Boolean loopBreak = false;
+                Boolean intersectFound = true;
+                if (singlePath.Length > 3)
+                {
+                    while (intersectFound)
+                    {
+                        intersectFound = false;
+                        Boolean loopBreak = false;
+                        for (int j = 0; j < singlePath.Length; j++)     //loop for String-inversion inside a path
+                        {
+                            if (loopBreak)
+                            {
+                                Array.Copy(singlePath, 0, points, leftOffset, rightOffset - leftOffset);
+                                break;
+                            }
+                            for (int k = 0; k < singlePath.Length; k++)
+                            {
+                                try
+                                {
+                                    if (singlePath[j] != singlePath[k] //there cant be intersections between the same 2 or 3 points
+                                        & singlePath[j + 1] != singlePath[k]
+                                        & singlePath[j] != singlePath[k + 1]
+                                        & singlePath[j + 1] != singlePath[k + 1])
+                                    {
+                                        bool intersect = isIntersect(singlePath[j], singlePath[j + 1], singlePath[k], singlePath[k + 1]);
+                                        if (intersect)
+                                        {
+                                            if (k > j)
+                                            {
+                                                Point[] tmp = new Point[Math.Abs(k - j)];
+                                                Array.Copy(singlePath, j + 1, tmp, 0, Math.Abs(k - j));
+                                                Array.Reverse(tmp);
+                                                Array.Copy(tmp, 0, singlePath, j + 1, Math.Abs(k - j));
+                                            }
+                                            else if (j > k)
+                                            {
+                                                Point[] tmp = new Point[Math.Abs(j - (k+1))];
+                                                Array.Copy(singlePath, k + 1, tmp, 0, Math.Abs(j - k));
+                                                Array.Reverse(tmp);
+                                                Array.Copy(tmp, 0, singlePath, k + 1, Math.Abs(j - k));
+                                            }
+                                            loopBreak = true;
+                                            intersectFound = true;
+                                            break;
+
+                                        }
+                                        else
+                                        {
+                                            intersectFound = false;
+                                        } //close IF
+                                    } //close IF
+                                }
+                                catch (IndexOutOfRangeException)
+                                {
+                                    intersectFound = false;
+                                }
+                            } //close inner for-loop (k)
+                        } //close outer for-loop (j)
+                    } //close while-loop
+                } //close IF                
+            } //close splitting for-loop (i)
+                return points;
+        } //close method singlePath
+
+        private static Boolean isIntersect(Point pointA, Point pointB, Point pointC, Point pointD)
+        {
+            Point A = new Point();
+            Point B = new Point();
+            Point C = new Point();
+            Point D = new Point();
+
+            A = pointA;
+            B = pointB;
+            C = pointC;
+            D = pointD;
+
+            float mAB = 0;          //gradient AB
+            float mCD = 0;          //gradient CD
+            float cAB = 0;          //y-intersection AB
+            float cCD = 0;          //y-intersection CD
+            float intersectX;       //intersection coordinate X
+            float intersectY;       //intersection coordinate Y
+
+            try
+            {
+                try
+                {
+                    //line A->B:
+                    mAB = Convert.ToSingle(B.Y - A.Y) / Convert.ToSingle(B.X - A.X);      //divide by zero! -> vertical line
+                    cAB = (A.Y - mAB * A.X);
+
+                    //line C->D
+                    mCD = Convert.ToSingle(D.Y - C.Y) / Convert.ToSingle(D.X - C.X);      //divide by zero! -> vertical line
+                    cCD = (C.Y - mCD * C.X);
+                }
+                catch (DivideByZeroException)
+                {
+                    MessageBox.Show("senkrechte Linie... Mist!");
+                }
+
+                try
+                {
+                    intersectX = ((cCD - cAB) / (mAB - mCD));   //divide by zero: same gradient -> no intersection
+                                                                //what if 2 lines are aligning?
+                    intersectY = mAB * intersectX + cAB;
+
+                    if ( (intersectX < A.X && intersectX > B.X) && (intersectX < C.X && intersectX > D.X ) )
+                    {
+                        return true;
+                    }
+                    else if ( (intersectX > A.X && intersectX < B.X) && (intersectX > C.X && intersectX < D.X) )
+                    {
+                        return true;
+                    }
+                    else if ((intersectX > A.X && intersectX < B.X) && (intersectX < C.X && intersectX > D.X))
+                    {
+                        return true;
+                    }
+                    else if ((intersectX < A.X && intersectX > B.X) && (intersectX > C.X && intersectX < D.X))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (DivideByZeroException)
+                {
+                    return false;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                return false;
+            }
+        }
+
+        public static Point[] completePath(Point[] points)
+        {
+
+            return points;
+        }
+
+    }
+
+    public class evo_optimization
+    {
+
+    }
+
+}
